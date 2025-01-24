c233587331960582fe24e77a91ecc48872196436
Harrison Field <hfield353@gmail.com>
2025-01-23 19:34:27 -0600

    main functionality complete

--- PacketSniffer/PacketSniffer.py
+++ PacketSniffer/PacketSniffer.py
@@ -6,6 +6,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Network packet sniffer') # Create a parser object this will handle the arguments
 parser.add_argument('--ip', help='IP address to sniff on', required=True) # Add an argument to the parser object to specify the IP address to sniff on
+parser.add_argument('--proto', help='Protocol to sniff (TCP/ICMP)', required=True) # Add an argument to the parser object to specify the IP address to sniff on
+parser.add_argument("--data", help='Display data', action='store_true') # Add an argument to the parser object to specify whether to display the data
 opts = parser.parse_args() # Parse the arguments
 
 class Packet:                   # Packet class
@@ -35,7 +37,7 @@ class Packet:                   # Packet class
         self.src_addr = ipaddress.ip_address(self.src) # Putting the source address in IP address format
         self.dst_addr = ipaddress.ip_address(self.dst) # Putting the destination address in IP address format
 
-        self.protocol_map = {1: "ICMP"} # Dictionary to map the protocol number to the protocol name
+        self.protocol_map = {1: "ICMP", 6: "TCP"} # Dictionary to map the protocol number to the protocol name
 
         try: # Try to get the protocol name from the dictionary
             self.protocol = self.protocol_map[self.pro] 
@@ -43,27 +45,40 @@ class Packet:                   # Packet class
             print(f'{e} No protocol for {self.pro}')
             self.protocol = str(self.pro)
 
-def print_header(self): # Print the packet header
-    print(f'Protocol: {self.protocol}') 
+    def print_header_short(self): # Print the packet header
+        print(f'Protocol: {self.protocol} {self.src_addr} -> {self.dst_addr}') # Print the packet header
 
+    def print_data(self): # Print the packet data
+        data = self.packet[20:] # Get the packet data we want to start from the 20th byte because the first 20 bytes are the header
+        print('*'*10 + 'ASCII START' + '*'*10) 
+        for b in data: # Iterate through the packet data
+            if b < 128:
+                print(chr(b), end='')
+            else:
+                print('.', end='')
+        print('*'*10 + 'ASCII END' + '*'*10)
 
 
 
 def sniff(host): 
-    socket_protocol = socket.IPPROTO_ICMP # Protocol number for ICMP
+    if opts.proto == 'tcp':
+        socket_protocol = socket.IPPROTO_TCP # Protocol for TCP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP # Protocol for ICMP
     sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol) # Create a raw socket to sniff on
     sniffer.bind((host, 0)) # Bind the socket to the host and port
-    sniffer.setsockopt(socket.IPPROTO_ICMP, socket.IP_HDRINCL, 1) # Include the IP header in the packet data
-
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1) # Include the IP header in the packet data
     try:
         while True:
-            raw_data = sniffer.recv(65535) 
-            packet = Packet(raw_data)
-            packet.print_header_short()
-    except KeyboardInterrupt:
+            raw_data = sniffer.recv(65535)  # Receive the packet data
+            packet = Packet(raw_data) # Create a Packet object with the packet data
+            packet.print_header_short() # Print the packet header
+            if opts.data:
+                packet.print_data() # Print the packet data if the --data argument is specified
+    except KeyboardInterrupt: # If Ctrl+C is pressed then exit the program
         print('Exiting...')
         sys.exit(1)
 
 
 if __name__ == '__main__': 
-    sniff() 
+    sniff(opts.ip) 
